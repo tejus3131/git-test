2053c6b6647bc4eaf165aa37e9f3a37e4a3d7c8c
Tejus Gupta <109463410+tejus3131@users.noreply.github.com>
2022-10-22 14:57:59 +0530

    branch commit vs code

--- test.cpp
+++ test.cpp
@@ -4,5 +4,6 @@ int main(){
     cout << "Hi I am Tejus Gupta. I am pursuing B. Tech. CSE(IoT).";
     cout << "commit reset 1";
     cout << "commit reset 2";
+    cout << "branch check";
     return 0;
 }
