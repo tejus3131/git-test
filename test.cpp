c5ebc8addf5cf85df12a6f9bb18329b6258d444e
Tejus Gupta <109463410+tejus3131@users.noreply.github.com>
2022-10-22 14:47:14 +0530

    commit2

--- test.cpp
+++ test.cpp
@@ -3,5 +3,6 @@ using namespace std;
 int main(){
     cout << "Hi I am Tejus Gupta. I am pursuing B. Tech. CSE(IoT).";
     cout << "commit reset 1";
+    cout << "commit reset 2";
     return 0;
 }
