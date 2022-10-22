0c4e428c350aa6789de54238a35db501d364a038
tejus3131 <tejusgupta3131@outlook.com>
2022-10-22 17:32:37 +0530

    nothing

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+from re import T
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -5,3 +6,6 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
