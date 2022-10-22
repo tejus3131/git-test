15e8dc3c7c09c328bc7033b31f857b870cc7f37c
tejus3131 <tejusgupta3131@outlook.com>
2022-10-22 15:08:29 +0530

    email check

--- test.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-int main(){
-    cout << "Hi I am Tejus Gupta. I am pursuing B. Tech. CSE(IoT).";
-    cout << "commit reset 1";
-    cout << "commit reset 2";
-    cout << "branch check";
-    return 0;
-}
