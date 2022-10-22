dac496c0e3894e3ce51de1d4d1a81161903be030
tejus3131 <tejusgupta3131@outlook.com>
2022-10-22 18:48:22 +0530

    Update test.cpp

---
+++ test.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout << "Hi I am Tejus Gupta. I am pursuing B. Tech. CSE(IoT).";
+    cout << "commit reset 1";
+    cout << "commit reset 2";
+    return 0;
+}
